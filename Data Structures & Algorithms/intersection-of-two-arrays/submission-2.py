4471419759e1e0ff5a54ba97a60072e3d2bbf11f
Saikiran <70024114+saikiran6694@users.noreply.github.com>
2026-04-28 17:35:12 -0230

    Add: intersection-of-two-arrays - submission-2

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = set()
+
+        for n1 in nums1:
+            for n2 in nums2:
+                if n1 == n2:
+                    res.add(n1)
+        return list(res)
